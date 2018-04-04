a1ff0fdec98a661d3320602b15c984531467fb8b
Rufat <rufatmdl@gmail.com>
2018-04-04 20:38:01 +0400

    Variable name change

--- src/client/src/routes.js
+++ src/client/src/routes.js
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import HTTP from './pages/HTTP'
 import Socket from './pages/Socket'
-import SemanticUI from './pages/Semantic'
+import Semantic from './pages/Semantic'
 
 export default class Main extends Component {
     render() {
@@ -13,7 +13,7 @@ export default class Main extends Component {
                     <Route exact path='/' component={Home}/>
                     <Route exact path='/HTTP' component={HTTP}/>
                     <Route path='/socket' component={Socket}/>
-                    <Route path='/semantic' component={SemanticUI}/>
+                    <Route path='/semantic' component={Semantic}/>
                     <Route component={() => '404 Not Found'} />
                 </Switch>
             </main>
